216605c0af6035d528682cf9417d2b94ce465bbd
Jack Wilson <JackWilson@jacks-macbook.bc.edu>
2018-11-25 19:29:02 -0500

    Initial Commit

---
+++ GoJournal/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GoJournal
+//
+//  Created by Jack Wilson on 11/25/18.
+//  Copyright © 2018 Jack Wilson. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
